96f594dae32b6957aa308b5f1666cdfbed8ff10c
Zsuzsanna <lengyel.zsuzsanna@gmail.com>
2025-03-26 20:17:32 +0100

    Add project files.

---
+++ Furdo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+public class Furdo
+{
+    public string Nev { get; }
+    public string Cim { get; }
+    public int Ar { get; }
+    public int Vizhofok { get; }
+
+    public Furdo(string sor)
+    {
+        string[] adatok = sor.Split(';');
+        Nev = adatok[0];
+        Cim = adatok[1];
+        Ar = int.Parse(adatok[2]);
+        Vizhofok = int.Parse(adatok[3]);
+    }
+
+    public string IRSZ()
+    {
+        return Cim.Substring(0, 4);
+    }
+
+    public string Telepules()
+    {
+        string[] reszek = Cim.Split(' ');
+        int vesszoIndex = Array.IndexOf(reszek, reszek.FirstOrDefault(r => r.Contains(',')));
+        if (vesszoIndex > 1)
+        {
+            return string.Join(" ", reszek.Skip(1).Take(vesszoIndex - 1));
+        }
+        else if (vesszoIndex == 1)
+        {
+            return reszek[1].TrimEnd(',');
+        }
+        return string.Empty; // Ha nem talál települést
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace StrandC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Furdo> furdok = BeolvasStrandAdatok("strandadatok.txt");
+
+            // 7. feladat
+            Console.WriteLine("7. feladat:");
+            Console.WriteLine($"Fürdők száma: {furdok.Count}");
+
+            // 8. feladat
+            Console.WriteLine("8. feladat");
+            double atlagAr = furdok.Average(f => f.Ar);
+            Console.WriteLine($"A fürdőbelépők átlagos ára: {atlagAr:F1}");
+
+            // 9. feladat
+            Console.WriteLine("9. feladat");
+            Furdo leghidegebb = furdok.OrderBy(f => f.Vizhofok).First();
+            Console.WriteLine($"A leghidegebb víz a(z) {leghidegebb.Nev} nevű fürdőben van.");
+
+            // 10. feladat
+            Console.WriteLine("10. feladat");
+            Console.Write("Kérem, adja meg a fürdő nevét!\n");
+            string keresettNev = Console.ReadLine();
+
+
+            Furdo keresettFurdo = furdok.FirstOrDefault(f => f.Nev == keresettNev);
+            if (keresettFurdo != null)
+            {
+                Console.WriteLine($"A fürdő {keresettFurdo.Telepules()} településen van, az irányítószám: {keresettFurdo.IRSZ()}");
+            }
+            else
+            {
+                Console.WriteLine("Nincs ilyen nevű fürdő!");
+            }
+
+            Console.ReadKey();
+        }
+
+        static List<Furdo> BeolvasStrandAdatok(string fajlnev)
+        {
+            List<Furdo> furdok = new List<Furdo>();
+            string[] sorok = File.ReadAllLines(fajlnev);
+            for (int i = 1; i < sorok.Length; i++) // Fejléc kihagyása
+            {
+                furdok.Add(new Furdo(sorok[i]));
+            }
+            return furdok;
+        }
+
+    }
+}
